620d769f7387fd6fd75872ebb6780a61eae9f554
Cara Capstick <caracapstick@gmail.com>
2023-03-29 15:43:47 +0100

    Implemented a simple state machine. Still not working though.

--- keypresses/key.v
+++ keypresses/key.v
@@ -1,5 +1,6 @@
 /* PMOD2 unused */
 module top(input CLK, PMODL1, PMODL2, output PMODL3, PMODL4, PMOD1, PMOD3, PMOD4, PMOD5, PMOD6, PMOD7, PMOD8 );
+
 	reg [6:0] leds;
 	assign PMOD7 = ~leds[0];
 	assign PMOD5 = ~leds[1];
@@ -8,24 +9,54 @@ module top(input CLK, PMODL1, PMODL2, output PMODL3, PMODL4, PMOD1, PMOD3, PMOD4
 	assign PMOD8 = ~leds[4];
 	assign PMOD6 = ~leds[5];
 	assign PMOD4 = ~leds[6];
+	
+	parameter CHECK_LINE1 = 1;
+	parameter CHECK_LINE2 = 2;
+	parameter CHECK_LINE3 = 4;
+	parameter CHECK_LINE4 = 8;
+
+	reg [3:0] state;
 
 	initial begin
 		leds <= 0;
+		state = CHECK_LINE1;
 	end
 	
 	always @(posedge CLK)
 	begin
-		PMODL4 <= 1;
-		PMODL3 <= 0;
-		if (PMODL1==1) begin leds <= 1;  end
-		else if (PMODL2==1) begin leds <= 2; end
-		else begin leds <= 127; end	
-		PMODL3 <= 1;
-		PMODL4 <= 0;
-		if (PMODL1==1) begin leds <= 16;  end
-		else if (PMODL2==1) begin leds <= 8; end
-		else begin leds <= 127; end
-
+		case( state )
+			CHECK_LINE1 : begin
+				PMODL3 = 1;
+				PMODL4 = 0;
+				if (PMODL1==1) begin
+					leds <= 1;
+				end
+				else if (PMODL2==1) begin
+					leds <= 2;
+				end
+				state = CHECK_LINE2;
+			end
+			CHECK_LINE2 : begin
+				PMODL3 = 0;
+				PMODL4 = 1;
+				if (PMODL1==1) begin
+					leds <= 4;
+				end
+				else if (PMODL2==1) begin
+					leds <= 8;
+				end
+				state = CHECK_LINE3;			
+			end
+			CHECK_LINE3 : begin
+				state = CHECK_LINE4;
+			end
+			CHECK_LINE4 : begin
+				// Restart line check loop next clock
+				state = CHECK_LINE1;
+			end
+			default	: begin end
+		endcase
+		
 	end
 endmodule
 
