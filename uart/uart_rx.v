f415c01c5d3fd8d0f898867a3a9e8448aca7b324
Cara Capstick <caracapstick@gmail.com>
2023-04-20 11:55:19 +0100

    Still writing uart module.

--- uart/uart_rx.v
+++ uart/uart_rx.v
@@ -12,34 +12,37 @@ module uart_rx (
 // Set your desired baud rate and correct FPGA clock rate
 parameter BAUD_RATE = 115200; // change as required
 parameter CLK_FREQ = 12000000; // change as required
-parameter TICKS_PER_UART_CLOCK_CYCLE = CLK_FREQ / BAUD_RATE - 1;
-parameter TICKS_PER_UART_HALF_CLOCK_CYCLE = COUNTER_MAX / TICKS_PER_UART_CLOCK_CYCLE;
+parameter [31:0] TICKS_PER_UART_MAX = CLK_FREQ / BAUD_RATE - 1;
+parameter [31:0] TICKS_PER_UART_HALF_CLOCK_CYCLE = COUNTER_MAX / TICKS_PER_UART_CLOCK_CYCLE;
 
 // UART states (keep track of two states, one for RX, one for TX)
-parameter STATE_WAIT			= 4'b0000;
-parameter STATE_START_BIT_FALLING_EDGE	= 4'b0001; // at beginning of start bit
-parameter STATE_START_BIT		= 4'b0010;
-parameter STATE_START_OF_DATA_BIT	= 4'b0011;
-parameter STATE_MIDDLE_OF_DATA_BIT	= 4'b0100;
-parameter STATE_STOP_BIT_RISING_EDGE	= 4'b0101; // at beginning of Stop bit
-parameter STATE_STOP_BIT		= 4'b0110;
-parameter STATE_ERROR			= 4'b0111;
-parameter STATE_RESET			= 4'b1000;
-parameter STATE_NEW_CTS			= 4'b1001;
-parameter STATE_NEW_RTS			= 4'b1010;
+parameter WAIT				= 4'b0000;
+parameter START_BIT_FALLING_EDGE	= 4'b0001; // at beginning of start bit
+parameter START_BIT			= 4'b0010;
+parameter START_OF_DATA_BIT		= 4'b0011;
+parameter MIDDLE_OF_DATA_BIT		= 4'b0100;
+parameter MIDDLE_OF_LAST_DATA_BIT	= 4'b0101;
+parameter STOP_BIT_RISING_EDGE		= 4'b0110; // at beginning of Stop bit
+parameter STOP_BIT			= 4'b0111;
+parameter ERROR				= 4'b1000;
+parameter RESET				= 4'b1001;
+parameter NEW_CTS			= 4'b1010;
+parameter NEW_RTS			= 4'b1011;
 
 reg [3:0] rx_state = STATE_WAIT;
 reg [3:0] tx_state = STATE_WAIT;
-
 
 reg [31:0] counter = 0;
 reg [3:0] bit_count = 0;
 reg state = 0; // 0: idle, 1: receiving
 
 
-always @(negedge rx and bit_count==7) begin
-	rx_state <= STATE_STOP_BIT_RISING_EDGE;
-	valid_byte <= 1; 
+always @(negedge rx) begin
+	if ( rx_state == STATE_MIDDLE_OF_LAST_DATA_BIT ) begin
+		rx_state <= STATE_STOP_BIT_RISING_EDGE;
+		valid_byte <= 1; 
+		counter <= TICKS_PER_UART_HALF_CLOCK_CYCLE; // in half a UART tick, check 
+	end
 end
 
 always @(posedge clk or posedge reset) begin
@@ -94,9 +97,7 @@ always @(posedge clk or posedge reset) begin
 end
 
 always @(posedge clk) begin
-    //if (data_valid) begin
-    //    data_valid <= 1'b0;
-   //end
+    counter <= counter + 1;
 end
 
 endmodule
